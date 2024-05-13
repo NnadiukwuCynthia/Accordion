80dd8474cf1e5b61e9991accc8ceb5e8006642b6
Cynthia Nnadiuwku <cynthiaamaran@gmail.com>
2024-05-13 07:11:32 +0100

    practiced event delegation

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
-const allAccordions = Array.from(document.querySelectorAll('.accordion'));
+// const allAccordions = Array.from(document.querySelectorAll('.accordion'));
+const accordionContainer = document.querySelector('.accordion-container')
 
-allAccordions.forEach( eachAccordion => {
-    const Header = eachAccordion.querySelector('.accordion__header')
+// allAccordions.forEach( eachAccordion => {
+//     const Header = eachAccordion.querySelector('.accordion__header')
 
-    Header.addEventListener('click', _ => {
-        eachAccordion.classList.toggle('is-open')
-    });
+//     Header.addEventListener('click', _ => {
+//         eachAccordion.classList.toggle('is-open')
+//     });
+// });
+
+accordionContainer.addEventListener('click', e => {
+    const Header = e.target.closest('.accordion__header');
+    if (Header) {
+        const accordion = Header.parentElement
+        accordion.classList.toggle('is-open')
+    }
 });
